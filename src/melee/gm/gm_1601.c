0903360816854856311bee46525f65323c3f4fc2
davidfeira <davidfeira@gmail.com>
2026-05-05 22:55:05 -0400

    Match fn_80162068

--- src/melee/gm/gm_1601.c
+++ src/melee/gm/gm_1601.c
@@ -832,13 +832,42 @@ f32 fn_80160F58(u8 ckind)
 
 /// #fn_80161C90
 
-/// #fn_80162068
+void fn_80162068(MatchEnd* match_end)
+{
+    s32 i;
+    s32 j;
+    struct MatchPlayerData* pdata_i;
+    struct MatchPlayerData* pdata_j;
+    struct FighterData* fd;
+    s32 sum;
+
+    for (i = 0; i < 4; i++) {
+        pdata_i = &match_end->player_standings[i];
+        if (pdata_i->slot_type == 3) {
+            continue;
+        }
+        fd = GetPersistentFighterData(gm_80164024(pdata_i->character_kind));
+        for (j = 0; j < 4; j++) {
+            pdata_j = &match_end->player_standings[j];
+            if (i == j || pdata_j->slot_type == 3) {
+                continue;
+            }
+            if (pdata_i->kills[j] + fd->fighter_kos[gm_80164024(pdata_j->character_kind)] > 0xFFFF) {
+                sum = 0xFFFF;
+            } else {
+                sum = pdata_i->kills[j] + fd->fighter_kos[gm_80164024(pdata_j->character_kind)];
+            }
+            fd->fighter_kos[gm_80164024(pdata_j->character_kind)] = (u16) sum;
+        }
+        fn_80161C90(match_end, i, &fd->sd_count);
+    }
+}
 
 /// #fn_80162170
 
 s32 gm_801623A4(MatchEnd* arg0)
 {
-    fn_80162068();
+    fn_80162068(arg0);
     return fn_80162170(arg0);
 }
 
@@ -3439,7 +3468,54 @@ void fn_80169900(u8 arg0, struct lbl_8046B488_t* arg1, s8* arg2, s8* arg3)
 
 /// #fn_80169C54
 
-/// #fn_80169F50
+void fn_80169F50(s8 arg0, s8 arg1)
+{
+    struct lbl_8046B488_t* gp = &lbl_8046B488;
+    s32 i;
+    s32 flag;
+    s8 v;
+    s8* p;
+
+    flag = 0;
+    if (gp->x0 == 4) {
+        flag = 1;
+    } else {
+        p = &gp->x1;
+        if (gp->x1 == 4) {
+            flag = 1;
+        } else if (p[1] == 4) {
+            flag = 1;
+        }
+    }
+
+    if (flag == 1 && gp->xE != 0) {
+        for (i = 0; gp->x20[i] != -2; i++) {
+            v = gp->x20[i];
+            if (v == -1) {
+                continue;
+            }
+            if (gp->xA2[i] != 4) {
+                continue;
+            }
+            if (gp->x124[i] == -1) {
+                continue;
+            }
+            Player_80031DA8(gp->x124[i], v);
+        }
+    }
+
+    if (arg0 == 4 && gp->xE != 0) {
+        for (i = 0; gp->x20[i] != -2; i++) {
+            if (gp->x20[i] == -1) {
+                continue;
+            }
+            if (gp->x124[i] == -1) {
+                continue;
+            }
+            Player_80031DA8(gp->x124[i], arg1);
+        }
+    }
+}
 
 void fn_8016A09C(void)
 {
